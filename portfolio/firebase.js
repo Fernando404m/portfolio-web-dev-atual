c0cad564ebfca85a2220b5e48147ae28f6719a16
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-07-26 18:39:16 -0300

    inicio do portfolio com react

---
+++ portfolio/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCstll0DFYrDB3PJF_JVKOjGlB-2yaaZnI",
+  authDomain: "portfolio-web-dev-fernando.firebaseapp.com",
+  projectId: "portfolio-web-dev-fernando",
+  storageBucket: "portfolio-web-dev-fernando.appspot.com",
+  messagingSenderId: "660894590818",
+  appId: "1:660894590818:web:8b498efeaa55ed35016e3b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
