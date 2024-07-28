180c8d2a0f989f9bdfbf5fc099c69c51625f4a2f
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-07-28 20:08:35 -0300

    nav bar completo

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-      <Nav></Nav>
       <Introducao />
+      <Nav />
     </>
   )
 }
--- portfolio/src/nav/nav.jsx
+++ portfolio/src/nav/nav.jsx
@@ -1,16 +1,34 @@
+import { useEffect } from "react"
 import "./nav.css"
 
 function Nav() {
+    let container;
+    
+    useEffect(() => {
+        container = document.querySelector(".nav-bar-btn-container")
+    }, [])
+
+    function mostrarNav() {
+        if (!container.classList.contains("nav-mostra")) {
+            container.classList.add("nav-mostra")
+            container.classList.remove("nav-oculta")
+        } else {
+            container.classList.add("nav-oculta")
+            container.classList.remove("nav-mostra")
+        }
+    }
+
     return(
-        <header>
+        <nav className="nav-bar">
             <h1></h1>
-            <nav className="nav-bar">
+            <button className="material-symbols-outlined" onClick={mostrarNav}>menu</button>
+            <div className="nav-bar-btn-container nav-oculta">
                 <button>Inicio</button>
                 <button>Sobre mim</button>
                 <button>Habilidades</button>
                 <button>Projetos</button>
-            </nav>
-        </header>
+            </div>
+        </nav>
     )
 }
 
