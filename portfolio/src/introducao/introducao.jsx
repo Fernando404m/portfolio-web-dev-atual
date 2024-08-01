dd29ae9452eb28530d11a788cdbdc0fda275124d
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-08-01 05:32:46 -0300

    iniciei a fazer o inicio

--- portfolio/src/introducao/introducao.jsx
+++ portfolio/src/introducao/introducao.jsx
@@ -4,8 +4,9 @@ function Introducao() {
     return(
         <header id="inicio">
             <div>
-                <h1>Fernando Marques</h1>
                 <img id="eu" />
+                <h1 id="nome" className="titulo">Fernando Marques</h1>
+                <h2 id="funcao" >Web Developer</h2>
             </div>
         </header>
     )
