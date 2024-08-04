6193255107e434f2ab8f21890b46d447f1b11ec9
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-08-04 13:25:14 -0300

    nome na introduçao

--- portfolio/src/introducao/introducao.jsx
+++ portfolio/src/introducao/introducao.jsx
@@ -1,12 +1,49 @@
 import "./introducao.css"
+import { useState, useEffect, useRef } from "react"
 
 function Introducao() {
+    const [nome, setnome] = useState("")
+    const [texto, setTexto] = useState("");
+    const fullname = "  Fernando Marques"
+    const fullText = "  Web Developer";
+    const index = useRef(0)
+
+    useEffect(() => {
+      function escreverNome() {
+        if (index.current < fullname.length) {
+          setnome(p => p + fullname.charAt(index.current));
+          index.current++;
+          setTimeout(escreverNome, 150);
+        }else {
+            index.current = 0
+            escrever()
+        }
+      }
+
+      function escrever() {
+        if (index.current === 1) {
+          document.getElementById("linha").classList.add("animacao-titulo")
+        }
+        if (index.current < fullText.length) {
+          setTexto(p => p + fullText.charAt(index.current));
+          index.current++;
+          setTimeout(escrever, 50);
+        }
+        
+      }
+
+      escreverNome()
+  }, [])
+
     return(
         <header id="inicio">
             <div>
                 <img id="eu" />
-                <h1 id="nome" className="titulo">Fernando Marques</h1>
-                <h2 id="funcao" >Web Developer</h2>
+                <div id="container">
+                    <div id="linha"></div>
+                    <h1 id="nome" className="titulo">{nome}</h1>
+                    <h2 id="funcao" >{texto}</h2>
+                </div>
             </div>
         </header>
     )
