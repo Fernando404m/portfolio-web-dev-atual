0a5010cef30a8d7bc1b4578423ed754bca7cf737
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-08-04 17:40:43 -0300

    mais sobre mim

--- portfolio/src/main/sobremim/sobremim.jsx
+++ portfolio/src/main/sobremim/sobremim.jsx
@@ -14,13 +14,43 @@ function sobremim() {
             document.getElementById("sobremim-resumo").classList.remove("animacao-sobremim-resumo")
             document.getElementById("sobremim-resumo").classList.add("animacao-sobremim-resumo-reverso")
         }
-
     })
+
+    function mostrarMais() {
+        const mostramais = document.getElementById("mais-sobremim")
+        if (mostramais.style.display == "block") {
+            mostramais.style.display = "none"
+        } else {
+            mostramais.style.display = "block"
+        }
+    }
 
     return(
         <div id="container-sobremim">
+            <div id="mais-sobremim">
+                <button id="close" className="material-symbols-outlined" onClick={mostrarMais}>close</button>
+                <h1>Linha do tempo</h1>
+                <div>
+                    <div>16 python curso em video</div>
+                    <div>17 projetos de automoçao de jogos com python</div>
+                    <div>17html e css do curso em video</div>
+                    <div>17 primeiro portfolio</div>
+                    <div>17 fiz o curso do freecodecamp</div>
+                    <div>17 aprendi js com o brocode</div>
+                    <div>17 segundo portfolio</div>
+                    <div>17 descobri como usar os serviços da hostinger e sobre os endereços</div>
+                    <div>parei para estudar outros assuntos, alem de desenvolver meu ingles</div>
+                    <div>18 retomei os estudos</div>
+                    <div>18 aprendi react com o brocode</div>
+                    <div>18 entendi mais sobre git e githup</div>
+                    <div>18 fiz esse portfolio</div>
+                </div>
+            </div>
             <div id="sobremim-resumo">
-                <h1>Sobre min</h1>
+                <h1>Sobre mim</h1>
+                <p>Meu nome é Fernando marques, sou um dev FullStack tentando iniciar minha carreira.</p>
+                <p>No momento estou tentando me desenvolver no mundo tecnologico pois sempre tive um fascinio sobre ele.</p>
+                <button id="mais-sobremim-btn" onClick={mostrarMais}>mais</button>
             </div>
         </div>
     )
