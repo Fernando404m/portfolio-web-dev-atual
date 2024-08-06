e5bb6388bb721979263489580c1065410eed9013
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-08-06 05:05:34 -0300

    habilidades 1

--- portfolio/src/main/habilidades/habilidades.jsx
+++ portfolio/src/main/habilidades/habilidades.jsx
@@ -3,7 +3,39 @@ import "./habilidades.css"
 function Habilidades() {
     return(
         <div id="habilidades">
-            <div></div>
+            <h1>Habilidades</h1>
+            <div id="habilidades-container">
+                <div className="hab" id="html">
+                    <div>
+                        <img src="src\assets\img\html.png" alt="HTML logo" />
+                        <h2>HTML</h2>
+                    </div>
+                </div>
+                <div className="hab" id="js">
+                    <div>
+                        <img src="src\assets\img\javascript.png" alt="JavaScript logo" />
+                        <h2>JavaScript</h2>
+                    </div>
+                </div>
+                <div className="hab" id="css">
+                    <div>
+                        <img src="src\assets\img\css.png" alt="CSS logo" />
+                        <h2>CSS</h2>
+                    </div>
+                </div>
+                <div className="hab" id="react">
+                    <div>
+                        <img src="src\assets\img\react.png" alt="react logo" />
+                        <h2>React</h2>
+                    </div>
+                </div>
+                <div className="hab" id="ingles">
+                    <div>
+                        <img src="src\assets\img\usa.png" alt="bandeira dos eua" />
+                        <h2>React</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
