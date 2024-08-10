94d132546574266585d4ed8efd8cd03c8ccaff63
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-08-10 08:44:42 -0300

    habilidades3

--- portfolio/src/main/habilidades/habilidades.jsx
+++ portfolio/src/main/habilidades/habilidades.jsx
@@ -2,9 +2,17 @@ import "./habilidades.css"
 
 function Habilidades() {
     function mostrar(item) {
-        document.getElementById("selecione_hab").style.display = "none"
-        document.querySelectorAll(".display_hab_text").forEach(iten => iten.style.display = "none")
-        document.getElementById(item + "_on").style.display = "block"
+        let display = document.getElementById("display-hab")
+        display.classList.remove("animacao-hab")
+        document.getElementById("selecione-hab").style.display = "none"
+        document.querySelectorAll(".display-hab-text").forEach(iten => {
+            iten.style.display = "none"
+        })
+        setTimeout(() => {
+            document.getElementById(item + "-on").style.display = "block"
+            display.classList.add("animacao-hab")
+        }, 10)
+        
     }
     return(
         <div id="habilidades">
@@ -41,28 +49,28 @@ function Habilidades() {
                     </div>
                 </button>
 
-                <div id="display_hab">
-                    <div class="display_hab_text" id="html_on">
+                <div id="display-hab">
+                    <div class="display-hab-text" id="html-on">
                         <h1>HTML5</h1>
                         <p>O <strong>HTML</strong> (HyperText Markup Language) é a linguagem de marcação mais usada na criação de sites, devido à sua semântica poderosa que auxilia o sistema de busca do Google. <br/>o estudo sobre sua semântica é muito impotante, para facilitar o design de aplicações web.</p>
                     </div>
-                    <div class="display_hab_text" id="css_on">
+                    <div class="display-hab-text" id="css-on">
                         <h1>CSS</h1>
                         <p>O <strong>CSS</strong> (Cascading Style Sheets) é o que define os visuais do <strong onClick={() => mostrar('html')}>HTML</strong>, em outras palavras, é o componente essencial para a criação de designs dos elementos <strong onClick={() => mostrar('html')}>HTML</strong>. <br/>Eu particularmente sempre busco manter um site organizado, responsivo e adaptável ao público.</p>
                     </div>
-                    <div class="display_hab_text" id="js_on">
+                    <div class="display-hab-text" id="js-on">
                         <h1>JavaScript</h1>
                         <p><strong>JavaScript</strong> é uma linguagem de programação que define funções a serem executadas em um site. Juntamente com o <strong onClick={() => mostrar('css')}>CSS</strong> e <strong onClick={() => mostrar('html')}>HTML</strong>, o JavaScript é uma das três principais tecnologias da internet.</p>
                     </div>
-                    <div class="display_hab_text" id="react_on">
+                    <div class="display-hab-text" id="react-on">
                         <h1>React</h1>
                         <p>O React é uma biblioteca do <strong onClick={() => mostrar("js")}>JavaScript</strong>, que comumente é utilizada para facilitar a criação de sites, devido a sua capacidade de criar componetes reutilizaveis (que são blocos de codigo isolados).</p>
                     </div>
-                    <div class="display_hab_text" id="ingles_on">
+                    <div class="display-hab-text" id="ingles-on">
                         <h1>Inglês</h1>
                         <p>Eu possuo um nível de <strong>inglês medio-avançado</strong>, suficiente para manter uma conversa, mas admito não ser totalmente fluente na língua.</p>
                     </div>
-                    <div id="selecione_hab">
+                    <div id="selecione-hab">
                         <h1>Selecione uma opção acima</h1>
                     </div>
                 </div>
