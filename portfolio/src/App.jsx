4569af074b301de2016aef76a5e6788733a8abea
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-08-10 15:41:41 -0300

    semi finalizado v mobile

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import "./app.css"
 import Introducao from "./introducao/introducao.jsx"
 import Nav from "./nav/nav"
 import Main from "./main/main.jsx"
+import Footer from "./footer/footer.jsx"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Introducao />
       <Nav />
       <Main />
+      <Footer />
     </>
   )
 }
---
+++ portfolio/src/footer/footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css"
+
+function Footer() {
+    return(
+        <footer id="footer">
+            <div id="email">email: fernando404m@gmail.com</div>
+            <div id="zap">numero: 19 999999999</div>
+            <div id="caixa-mensagem">voltar para o inicio</div>
+        </footer>
+    )
+}
+
+export default Footer
